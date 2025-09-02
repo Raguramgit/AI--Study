f93db25780ec10b93a071d1fdfd35bf1a41fb921
Raguraman Palani <rpalani@corp.logitech.com>
2025-09-02 10:45:55 +0530

    Mastering File Handling

--- test.py
+++ test.py
@@ -102,3 +102,12 @@ with open("note.txt", "r") as file:
     content = file.read()
     print(content)
     file.close() '''
+
+# Mastering File Handling in Python using OS and Importing Custom Modules
+import os
+os.getcwd()
+os.listdir()
+#os.mkdir("Code")
+from Code.package import add, sub
+print(add(5, 3))
+print(sub(5, 3))
