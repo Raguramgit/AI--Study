4938e8fd8768047d621123b0ccf91fa91b44d7cb
Raguraman Palani <rpalani@corp.logitech.com>
2025-09-03 12:29:45 +0530

    Keyboard Operation

--- RPA/pyautogui/rpa_demo.py
+++ RPA/pyautogui/rpa_demo.py
@@ -8,3 +8,8 @@ time.sleep(3)
 pyautogui.rightClick(100, 100)
 time.sleep(3)
 pyautogui.doubleClick(100, 1000)
+
+#Keyboard operations using pyautogui
+time.sleep(3)
+pyautogui.write("Hello from RPA with Python", interval=0.1)
+pyautogui.press("enter")
