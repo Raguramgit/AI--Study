33d841961799f5e5d7b5556551eb1e614d2c3af6
Raguraman Palani <rpalani@corp.logitech.com>
2025-09-03 15:49:16 +0530

    Keyboard operation

--- RPA/pyautogui/rpa_demo.py
+++ RPA/pyautogui/rpa_demo.py
@@ -3,13 +3,14 @@ import time
 print ("Hello from RPA with Python")
 
 #Mouse operations using pyautogui
-pyautogui.click(100, 100)
+''' pyautogui.click(100, 100)
 time.sleep(3)
-pyautogui.rightClick(100, 100)
+pyautogui.rightClick(100, 100)'''
 time.sleep(3)
-pyautogui.doubleClick(100, 1000)
+pyautogui.click(146, 356)
+
 
 #Keyboard operations using pyautogui
 time.sleep(3)
-pyautogui.write("Hello from RPA with Python", interval=0.1)
-pyautogui.press("enter")
+pyautogui.write("Hello from RPA with Python")
+pyautogui.hotkey("ctrl", "s")
